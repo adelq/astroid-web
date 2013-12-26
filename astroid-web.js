b73d873c9c9303817e317b583c82636320df2ba6
Adel Qalieh <aqalieh95@gmail.com>
2013-12-26 17:32:22 -0500

    Add basic add task functionality Move lots of previous code into templates Add event handlers for templates

--- astroid-web.js
+++ astroid-web.js
@@ -1,4 +1,34 @@
+Tasks = new Meteor.Collection("tasks");
+Lists = new Meteor.Collection("lists");
+Folders = new Meteor.Collection("folders");
+
 if (Meteor.isClient) {
+  Template.todos.tasks = function () {
+    return Tasks.find();
+  }
+
+  Template.newTaskForm.events({
+    'submit #new-task, click #addTaskButton': function(e) {
+      e.preventDefault();
+      var body = $('#new-task-text').val();
+      $('#new-task-text').val("");
+      var now = moment();
+      var priority = 'low';
+      var list = 'Home';
+      Tasks.insert({
+        body: body,
+        date: now.format('D MMM YYYY'),
+        dateDue: now,
+        dateCreated: now,
+        dateCompleted: false,
+        modified: now,
+        list: list,
+        priority: priority,
+        complete: false,
+        repeating: false
+      });
+    }
+  });
 }
 
 if (Meteor.isServer) {
